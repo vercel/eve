gorkking/eve-open-framework-for-building-agents

feat(eve): expose userspace sandbox drives
Signed-off-by: Rui Conti <ruiconti@gmail.com>

packages/eve/test/scenarios/public-api-portability.scenario.test.ts
@@ -46,7 +46,7 @@ const PORTABILITY_CASES: readonly PortabilityCase[] = [
 import { DockerSandbox } from "eve/sandbox/docker";
 import { JustBashSandbox } from "eve/sandbox/just-bash";
 import { MicrosandboxSandbox } from "eve/sandbox/microsandbox";
-import { VercelSandbox } from "eve/sandbox/vercel";
+import { Drive, VercelSandbox } from "eve/sandbox/vercel";
 import {
   defineSandboxAdapter,
   defineSandboxTemplate,
@@ -57,6 +57,7 @@ import {
 void DockerSandbox;
 void JustBashSandbox;
 void MicrosandboxSandbox;
+void Drive;
 void defineSandboxAdapter;
 void defineSandboxTemplate;
 void ({} as Sandbox);

vercel/eve

packages/eve/test/scenarios/public-api-portability.scenario.test.ts
@@ -46,7 +46,7 @@ const PORTABILITY_CASES: readonly PortabilityCase[] = [
 import { docker } from "eve/sandbox/docker";
 import { justbash } from "eve/sandbox/just-bash";
 import { microsandbox } from "eve/sandbox/microsandbox";
-import { vercel } from "eve/sandbox/vercel";
+import { Drive, vercel } from "eve/sandbox/vercel";
 
 const fallback = defaultBackend({
   docker: { image: "ghcr.io/vercel/eve:latest" },
@@ -54,6 +54,7 @@ const fallback = defaultBackend({
   microsandbox: {},
   vercel: { resources: { vcpus: 2 } },
 });
+void Drive;
 
 void docker;
 void justbash;