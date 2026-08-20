532037d7d4298fb438e9cfda1e65097ba79eca04
Rui Conti <ruiconti@gmail.com>
2026-08-20 17:10:57 -0400

    chore(eve): preserve channel extension compatibility
    Signed-off-by: Rui Conti <ruiconti@gmail.com>

--- packages/eve/src/compiler/extension-compatibility.ts
+++ packages/eve/src/compiler/extension-compatibility.ts
@@ -31,7 +31,7 @@ const EXTENSION_CAPABILITY_CONTRACTS = {
     supported: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
     dropped: {},
   },
-  channel: { current: 5, supported: [1, 2, 3, 4, 5], dropped: {} },
+  channel: { current: 6, supported: [1, 2, 3, 4, 5, 6], dropped: {} },
   schedule: { current: 3, supported: [1, 2, 3], dropped: {} },
   subagent: { current: 2, supported: [1, 2], dropped: {} },
   connection: { current: 5, supported: [1, 2, 3, 4, 5], dropped: {} },
