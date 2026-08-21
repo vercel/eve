a3666ecb4c69590682c75a890dcbb6e6af62d8c1
Felix Arntz <felix.arntz@vercel.com>
2026-08-21 10:27:25 -0500

    artifacts

--- packages/eve/src/compiler/extension-compatibility.ts
+++ packages/eve/src/compiler/extension-compatibility.ts
@@ -21,7 +21,7 @@ interface ExtensionCapabilityContract {
 
 const EXTENSION_CAPABILITY_CONTRACTS = {
   extension: { current: 1, supported: [1], dropped: {} },
-  tool: { current: 13, supported: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], dropped: {} },
+  tool: { current: 14, supported: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], dropped: {} },
   dynamicTool: {
     current: 18,
     supported: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
