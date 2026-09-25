Caceras/eve-template

feat(eve): delegate Connect manifest compilation
Signed-off-by: benpankow <ben.pankow@vercel.com>

packages/eve/src/framework/sources/registry.ts
@@ -9,7 +9,7 @@ import {
   type ProgrammaticModuleNamespace,
 } from "#compiler/source-graph.js";
 
-const revision = `eve@${resolveInstalledPackageInfo().version}:compiled-manifest-v48`;
+const revision = `eve@${resolveInstalledPackageInfo().version}:compiled-manifest-v49`;
 
 const localDefaults = defineProgrammaticAgentSource({
   id: "eve:defaults",

vercel/eve

packages/eve/src/framework/sources/registry.ts
@@ -9,7 +9,7 @@ import {
   type ProgrammaticModuleNamespace,
 } from "#compiler/source-graph.js";
 
-const revision = `eve@${resolveInstalledPackageInfo().version}:compiled-manifest-v50`;
+const revision = `eve@${resolveInstalledPackageInfo().version}:compiled-manifest-v52`;
 
 const localDefaults = defineProgrammaticAgentSource({
   id: "eve:defaults",